15e38f69adf076fa21cd99fd3935e8f98140b201
seamoooo <hm.seamo@gmail.com>
2022-10-13 11:17:25 +0900

    add question all

--- flutter/flutter_complete_guide/lib/main.dart
+++ flutter/flutter_complete_guide/lib/main.dart
@@ -7,8 +7,53 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  // 通常の関数宣言をしてしまうとMyappのクラスメソッドになってしまう。
+  var questionIndex = 0;
+
+  void anserQuestion() {
+    questionIndex = questionIndex + 1;
+    print(questionIndex);
+  }
+
   @override // flutterので用意されているstatemant 上書きしている際に書くことを推奨左rて言える
   Widget build(BuildContext context) {
-    return MaterialApp(home: Text('hello'));
+    var question = [
+      'what\'s favarit clolor',
+      'what\s favorit animals',
+    ];
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text("hello"),
+          ),
+          body: Column(
+            children: [
+              Text(
+                question[questionIndex],
+              ),
+              ElevatedButton(
+                child: Text('anser 1'),
+                // 下記の場合だと関数の戻り値(void)を渡してしまっているので、エラーになる
+                // onPressed: anserQuestion(),
+                // 下記のようにポインタを渡す必要がある
+                onPressed: anserQuestion,
+              ),
+              ElevatedButton(
+                child: Text('anser 2'),
+                //　アロー関数で呼び出すこともできる
+                onPressed: () => print("hoge"),
+              ),
+              ElevatedButton(
+                child: Text('anser 3'),
+                // プロパティに対して直接関数を設定することも可能
+                onPressed: () {
+                  /////// ----
+                  ///
+                  print("直接関数を設定できる");
+                },
+              ),
+            ],
+          )),
+    );
   }
 }
