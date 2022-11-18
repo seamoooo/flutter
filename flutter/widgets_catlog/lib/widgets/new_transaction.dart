ee284da6c05a31e6d40dadb81907a4222912f970
seamoooo <hm.seamo@gmail.com>
2022-11-18 17:33:14 +0900

    buttonの追加

--- flutter/widgets_catlog/lib/widgets/new_transaction.dart
+++ flutter/widgets_catlog/lib/widgets/new_transaction.dart
@@ -1,28 +1,34 @@
 import 'package:flutter/material.dart';
 
-class NewTransaction extends StatelessWidget {
+class NewTransaction extends StatefulWidget {
   final Function addTx;
-  final titleController = TextEditingController();
-  final amountController = TextEditingController();
 
   NewTransaction(this.addTx);
+
+  @override
+  State<NewTransaction> createState() => _NewTransactionState();
+}
+
+// ステートレスwidgetにとって、値を再評価するたびに、データはリセットされる
+class _NewTransactionState extends State<NewTransaction> {
+  final titleController = TextEditingController();
+
+  final amountController = TextEditingController();
 
   void submitData() {
     final enteredTitle = titleController.text;
     final enteredAmount = double.parse(amountController.text);
 
-    print("test");
-
     if (enteredTitle.isEmpty || enteredAmount <= 0) {
       return;
     }
 
-    print("test2");
-
-    addTx(
+    widget.addTx(
       enteredTitle,
       enteredAmount,
     );
+
+    Navigator.of(context).pop();
   }
 
   @override
